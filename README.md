f687a70757581f77e97556d7216582c17b40f290
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-10-01 02:31:53 +0300

    Allow building grouped-by data files

--- README.md
+++ README.md
@@ -55,6 +55,80 @@ async function getData(): JSXml<JSXml[]> {
 }
 ```
 
+## Grouping entries
+
+To group entries by a criterion (see available criteria below), a corresponding environment variable must be set in the process that installs the package (i.e., the process that does `npm i @iso4217/json`, or `npm i`, or `npm ci`). Most values of a environment variable will result in the corresponding file being forcefully rebuild, but special keywords can provide a way to tweak this behavior (see available behaviors below).
+
+### Available groupings
+
+- by country name (by `<CtryNm>` tag):
+  - env: `ISO4217_JSON_BUILD_DATA_GROUPED_BY_COUNTRY_NAME`
+  - builds file `data-grouped-by-country-name.json`
+- by currency name (by `<CcyNm>` tag):
+  - env: `ISO4217_JSON_BUILD_DATA_GROUPED_BY_CURRENCY_NAME`
+  - builds file `data-grouped-by-currency-name.json`
+- by currency code (by `<Ccy>` tag):
+  - env: `ISO4217_JSON_BUILD_DATA_GROUPED_BY_CURRENCY_CODE`
+  - builds file `data-grouped-by-currency-code.json`
+- by currency number (by `<CcyNbr>` tag):
+  - env: `ISO4217_JSON_BUILD_DATA_GROUPED_BY_CURRENCY_NUMBER`
+  - builds file `data-grouped-by-currency-number.json`
+- by currency minor units (by `<CcyMnrUnts>` tag):
+  - env: `ISO4217_JSON_BUILD_DATA_GROUPED_BY_CURRENCY_MINOR_UNITS`
+  - builds file `data-grouped-by-currency-minor-units.json`
+
+### Available behaviors
+
+- _never build the file_
+
+  Invoked by `"0"`, `"false"`, and `"never"`; default behavior (i.e., when variable is unset or empty)
+
+- _build the file only if it doesn't exist already; otherwise skip_
+
+  Invoked by `"2"`, `"soft"`, `"if-not-exists"`, and `"unless-exists"`;
+
+- _build the file only if it exists already; otherwise skip_
+
+  Invoked by `"3"`, and `"if-exists"`;
+
+- _always build the file, regardless of whether it already exists or not_
+
+  Invoked by `"1"`, `"true"`, `"always"`, and any non-empty string (except for the ones listed here).
+
+### Example
+
+This command:
+
+```sh
+# (using bash syntax)
+
+export ISO4217_JSON_BUILD_DATA_GROUPED_BY_CURRENCY_NAME=2 # the file exists
+export ISO4217_JSON_BUILD_DATA_GROUPED_BY_CURRENCY_CODE=1
+export ISO4217_JSON_BUILD_DATA_GROUPED_BY_CURRENCY_NUMBER=1
+npm install
+```
+
+&hellip; logs this output (order is not guaranteed):
+
+```sh
+# @iso4217/json: Skipping file "@iso4217/json/data-grouped-by-currency-name.json" (strategy "if-not-exists")
+# @iso4217/json: Building file "@iso4217/json/data-grouped-by-currency-code.json" ...
+# @iso4217/json: Building file "@iso4217/json/data-grouped-by-currency-number.json" ...
+# @iso4217/json: File "@iso4217/json/data-grouped-by-currency-number.json" is built successfully
+# @iso4217/json: File "@iso4217/json/data-grouped-by-currency-code.json" is built successfully
+```
+
+Note, that some groupings don't include particular currencies, – for the most part, exotic ones. For example, Antarctica does not have any universal currency, and the XML file doesn't have `<Ccy>` tag for it. Therefore, Antarctica won't appear anywhere in the "grouped by currency code" file. However, it will appear in "grouped by country name" file, since "ANTARCTICA" is considered a country name by [maintainers][2] of the XML file.
+
+Also note, that to manually build the files after the package is already installed (in case you forgot to set up environment variables, for example), after properly setting the environment, you should then either run `npm i` / `npm ci`, or run `node_modules/@iso4217/json/build-grouped-by-data-files.js` file with Node.JS executable (`node`); usually it looks like this:
+
+```sh
+# (using bash syntax)
+
+export ... # setting environment variables
+node node_modules/@iso4217/json/build-grouped-by-data-files.js
+```
+
 ## Versioning strategy
 
 _TL;DR: Major version repeats XML's `Pblshd` attribute._
---
+++ build-grouped-by-data-files.ts
@@ -0,0 +1,176 @@
+import fs from "fs";
+import packageJson from "./package.json";
+import type { JSXml, Primitive } from "./js-xml.type";
+import json from "./data.json";
+import { writeJsonToFile } from "./write-to-file";
+
+/** @private */
+const envPrefix = "ISO4217_JSON_BUILD_DATA_GROUPED_BY_";
+
+/** @private */
+const grouping = {
+	COUNTRY_NAME: {
+		tagName: "CtryNm",
+		fileName: "data-grouped-by-country-name.json",
+	},
+	CURRENCY_NAME: {
+		tagName: "CcyNm",
+		fileName: "data-grouped-by-currency-name.json",
+	},
+	CURRENCY_CODE: {
+		tagName: "Ccy",
+		fileName: "data-grouped-by-currency-code.json",
+	},
+	CURRENCY_NUMBER: {
+		tagName: "CcyNbr",
+		fileName: "data-grouped-by-currency-number.json",
+	},
+	CURRENCY_MINOR_UNITS: {
+		tagName: "CcyMnrUnts",
+		fileName: "data-grouped-by-currency-minor-units.json",
+	},
+} as const;
+
+/** @private */
+type Grouping = typeof grouping;
+
+/** @private */
+type EnvKeyPostfix = keyof Grouping;
+
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv extends Partial<Record<`${typeof envPrefix}${EnvKeyPostfix}`, string>> {}
+	}
+}
+
+const values = {
+	0b0: false,
+	0b1: true,
+	0b10: "if-not-exists",
+	0b11: "if-exists",
+
+	true: true,
+	false: false,
+
+	always: true,
+	never: false,
+
+	hard: true,
+	soft: "if-not-exists",
+
+	"if-exists": "if-exists",
+	"if-not-exists": "if-not-exists",
+	"unless-exists": "if-not-exists",
+} as const;
+
+/** @private */
+type SpecialValues = typeof values;
+
+/** @private */
+type SpecialValue = keyof SpecialValues;
+
+/** @private */
+type BuildStrategy = SpecialValues[SpecialValue];
+
+/** @private */
+function getBuildStrategy(value: string | undefined): BuildStrategy {
+	if (value == null)
+		return false;
+
+	if (value in values)
+		return values[value as SpecialValue];
+
+	return !!value;
+}
+
+/** @private */
+type Entry = JSXml<JSXml[]>;
+
+/** @private */
+type EntryDataItem = JSXml<Primitive>;
+
+/** @private */
+const entries: Entry[] = json.$data[0].$data;
+
+/** @private */
+function getEntryDataItem(entry: Entry, itemName: Grouping[EnvKeyPostfix]["tagName"]): EntryDataItem | null {
+	for (const item of entry.$data)
+		if (item.$name === itemName)
+			return item as EntryDataItem;
+
+	return null;
+}
+
+/** @private */
+function getEntriesGroupedBy(envKeyPostfix: EnvKeyPostfix): Record<string, Entry[]> {
+	const { tagName } = grouping[envKeyPostfix];
+	const grouped = Object.create(null) as Record<string, Entry[]>;
+
+	for (const entry of entries) {
+		const dataItem = getEntryDataItem(entry, tagName);
+
+		if (dataItem == null)
+			continue;
+
+		const groupName = String(dataItem.$data);
+
+		if (groupName in grouped)
+			grouped[groupName].push(entry);
+
+		else
+			grouped[groupName] = [ entry ];
+	}
+
+	return grouped;
+}
+
+/** @private */
+const logPrefix = `${packageJson.name}:`;
+
+/** @private */
+function log(level: "info" | "error", ...values: unknown[]) : void {
+	console[level](logPrefix, ...values);
+}
+
+export default async function buildGroupedByDataFiles() {
+	const jobs: Promise<void>[] = [];
+
+	for (const envKeyPostfix in grouping) {
+		const { fileName } = grouping[envKeyPostfix as EnvKeyPostfix];
+		const envVarKey = envPrefix + envKeyPostfix;
+		const envVarValue = process.env[envVarKey];
+		const strategy = getBuildStrategy(envVarValue);
+
+		if (strategy === false)
+			continue;
+
+		const filePath = `${packageJson.name}/${fileName}`;
+
+		if (strategy !== true) {
+			const isIfExists = strategy === "if-exists";
+			const fileExists = fs.existsSync(fileName);
+
+			if (isIfExists !== fileExists) {
+				log("info", `Skipping file "${filePath}" (strategy "${strategy}")`);
+				continue;
+			}
+		}
+
+		log("info", `Building file "${filePath}" ...`);
+
+		const dataGrouped = getEntriesGroupedBy(envKeyPostfix as EnvKeyPostfix);
+		const write = writeJsonToFile(fileName, dataGrouped);
+		const job = write.then(() => {
+			log("info", `File "${filePath}" is built successfully`);
+		}, (error) => {
+			log("error", `An error encountered while building "${filePath}":`, error);
+		});
+
+		jobs.push(job);
+	}
+
+	await Promise.all(jobs);
+}
+
+if (require.main === module)
+	buildGroupedByDataFiles();
