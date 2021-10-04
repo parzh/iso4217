1137eb7893345205638a052b8b28df07c2686219
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-10-04 12:56:36 +0300

    Write dedicated postinstall script file

---
+++ postinstall.js
@@ -0,0 +1,36 @@
+"use strict";
+const producerPackageName = require("./package.json").name; // expected to see "@iso4217/json"
+const consumerPackageName = process.env.npm_package_name ?? ""; // expected to see arbitrary package name
+
+const logPrefix = `${producerPackageName}:`;
+
+/** @param {unknown[]} values @returns {void} */
+function log(...values) {
+	console.log(logPrefix, "[meta]", ...values);
+}
+
+function postinstall() {
+	if (consumerPackageName === producerPackageName) {
+		const fs = require("fs");
+
+		if (!fs.existsSync("./build-grouped-by-data-files.js")) {
+			const { spawnSync } = require("child_process");
+
+			log("Compiling script file ...");
+
+			const { error } = spawnSync("npm", [ "run", "build" ], {
+				cwd: process.cwd(),
+				timeout: 20000,
+			});
+
+			if (error)
+				throw error;
+
+			log("Script file has been compiled");
+		}
+	}
+
+	require("./build-grouped-by-data-files").default();
+}
+
+postinstall();
