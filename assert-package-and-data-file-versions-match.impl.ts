f80d876083b82211a7d69287b65ca69e270b4b9d
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-10-04 14:58:35 +0300

    Compare versions before publishing

---
+++ assert-package-and-data-file-versions-match.impl.ts
@@ -0,0 +1,19 @@
+import { version as actual } from "./package.json";
+import composeVersionNumber from "./compose-version-number.impl";
+
+export class VersionMismatchError extends global.Error {
+	constructor(public actual: string, public expected: string) {
+		super(`Package version ("${actual}") does not match data version ("${expected}")`);
+	}
+}
+
+export default function assertPackageAndDataFileVersionsMatch(): void {
+	const expected = composeVersionNumber();
+	const equals = expected === actual;
+
+	if (!equals)
+		throw new VersionMismatchError(actual, expected);
+}
+
+if (require.main === module)
+	assertPackageAndDataFileVersionsMatch();
--- compose-version-number.impl.ts
+++ compose-version-number.impl.ts
@@ -1,12 +1,30 @@
 import data from "./data.json";
 import packageJson from "./package.json";
 
+/** @private */
+interface DateSimple {
+	year: string;
+	month: string;
+	day: string;
+}
+
+/** @private */
+function getDataVersionComponents(dataVersion: string): DateSimple {
+	const date = new Date(dataVersion); // assuming that `dataVersion` is a ISO-8601-compliant string 😡
+
+	return {
+		year: String(date.getFullYear()),
+		month: String(date.getMonth() + 1),
+		day: String(date.getDate()),
+	};
+}
+
 export default function composeVersionNumber(): string {
-	const major = data.$attr.Pblshd.replace(/\D/g, "");
+	const { year, month, day } = getDataVersionComponents(data.$attr.Pblshd);
 	const [ version ] = packageJson.version.split(/[+-]/, 1);
 	const [ , minor, patch ] = version.split(".");
 
-	const result = `${major}.${minor}.${patch}`;
+	const result = `${year}${month}${day}.${minor}.${patch}`;
 
 	return result;
 }
