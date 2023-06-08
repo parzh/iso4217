ce88ce1ed4c7c7097ac58545ad1d4ffc4e374f36
Dima Parzhitsky <parzhitsky@gmail.com>
2023-06-08 18:18:44 +0300

    Fix negation mistake

--- build-data-file-if-needed.impl.ts
+++ build-data-file-if-needed.impl.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import buildDataFile from "./build-data-file.impl";
 
 export default async function buildDataFileIfNeeded(): Promise<void> {
-	if (fs.existsSync('./data.json')) {
+	if (!fs.existsSync('./data.json')) {
 		await buildDataFile()
 	}
 }
